f01d6989751ea25a46c58864089cc495946355fe
chyyuu <yuchen@mail.tsinghua.edu.cn>
2019-07-21 12:47:44 +0800

    Hello World! -- step 2：设置堆栈

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,8 +1,11 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(global_asm)]
 
 use core::panic::PanicInfo;
 use bbl::sbi;
+
+global_asm!(include_str!("boot/entry.asm"));
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -17,4 +20,9 @@ pub extern "C" fn rust_main() -> ! {
         sbi::console_putchar(c as usize);
     }
     loop {}
+}
+
+#[no_mangle]
+pub extern fn abort() {
+    panic!("abort!");
 }
