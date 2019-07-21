2122c6db5a8d46a33c91b36a6f23243a812fabe6
chyyuu <yuchen@mail.tsinghua.edu.cn>
2019-07-21 16:53:18 +0800

    显示机器和内核的内存信息

--- os/src/init.rs
+++ os/src/init.rs
@@ -1,6 +1,7 @@
 use crate::interrupt::init as interrupt_init;
 use crate::clock::init as clock_init;
 use crate::memory::init as memory_init;
+use crate::consts::*;
 
 global_asm!(include_str!("boot/entry.asm"));
 
@@ -16,5 +17,16 @@ pub extern "C" fn rust_main(hartid: usize, dtb: usize) -> ! {
       let x = *x_ptr;
       println!("addr:{:?}: content:0x{:x} ",x_ptr, x);
     }
+    let  dtb_info = device_tree::DeviceTree::dtb_query_memory(dtb);
+    match dtb_info {
+       Some((addr, mem_size))=> {    
+           // 内核的起始物理地址
+           let kernel_end = dtb - KERNEL_OFFSET + MEMORY_OFFSET + PAGE_SIZE;
+           // 内核的终止物理地址
+           let kernel_size = kernel_end - addr;
+           println!("addr:0x{:x}: mem_size:0x{:x} ",addr, mem_size);
+           println!("kernel_end:0x{:x}: kernel_size:0x{:x} ",kernel_end, kernel_size);},
+	   None => {}
+    } 
     loop {}
 }	
