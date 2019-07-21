9edb00f38a00d8a4811c42f493edced7e3892964
chyyuu <yuchen@mail.tsinghua.edu.cn>
2019-07-21 20:42:34 +0800

    实现页表

--- os/src/memory/mod.rs
+++ os/src/memory/mod.rs
@@ -1,4 +1,5 @@
 pub mod frame_allocator;
+mod paging;
 
 use self::frame_allocator::{init as init_frame_allocator, test as test_frame_allocator};
 use crate::consts::*;
@@ -13,6 +14,7 @@ pub fn init() {
     init_heap();
     init_frame_allocator();
     test_frame_allocator();
+    remap_kernel();
 }
 
 fn init_heap() {
@@ -23,4 +25,52 @@ fn init_heap() {
             .init(HEAP.as_ptr() as usize, KERNEL_HEAP_SIZE);
     }
     println!("heap init end");
+}
+
+extern "C" {
+    // text
+    fn stext();
+    fn etext();
+    // data
+    fn sdata();
+    fn edata();
+    // read only
+    fn srodata();
+    fn erodata();
+    // bss
+    fn sbss();
+    fn ebss();
+    // kernel
+    fn start();
+    fn end();
+    // boot
+    fn bootstack();
+    fn bootstacktop();
+}
+
+fn remap_kernel() {
+    println!("remaping");
+    let offset = KERNEL_OFFSET as usize - MEMORY_OFFSET as usize;
+    use crate::memory::paging::{InactivePageTable, MemoryAttr};
+    let mut pg_table = InactivePageTable::new(offset);
+    pg_table.set(
+        stext as usize,
+        etext as usize,
+        MemoryAttr::new().set_readonly().set_execute(),
+    );
+    pg_table.set(sdata as usize, edata as usize, MemoryAttr::new().set_WR());
+    pg_table.set(
+        srodata as usize,
+        erodata as usize,
+        MemoryAttr::new().set_readonly(),
+    );
+    pg_table.set(sbss as usize, ebss as usize, MemoryAttr::new().set_WR());
+    pg_table.set(
+        bootstack as usize,
+        bootstacktop as usize,
+        MemoryAttr::new().set_WR(),
+    );
+    unsafe {
+        pg_table.activate();
+    }
 }
