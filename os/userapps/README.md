ef260241b0c7c8bc6982e11aefd608dd10d1f30a
chyyuu <yuchen@mail.tsinghua.edu.cn>
2019-07-21 14:44:37 +0800

    应用程序中变量的虚拟地址

---
+++ os/userapps/README.md
@@ -0,0 +1,10 @@
+## 一下应用程序例子
+### 显示变量的虚拟地址
+- showaddr.rs
+
+
+## 编译APPNAME.rs的运行方法
+```
+rustc APPNAME.rs
+./APPNAME
+```
---
+++ os/userapps/showaddr.rs
@@ -0,0 +1,6 @@
+fn main(){
+    let a:i32 = 5;
+    //&a先转成raw指针，然后再把指针转成usize，这样就可以显示地址了。
+    let addr = &a as *const i32 as usize;
+    println!("addr：0x{:X}",addr);
+}
