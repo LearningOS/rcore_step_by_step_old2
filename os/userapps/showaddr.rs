3addfae0424210d725eaa364c7f48e89814d5b06
chyyuu <yuchen@mail.tsinghua.edu.cn>
2019-07-21 15:27:15 +0800

    show raw pointer itself and the content of raw pointer pointed in user apps

--- os/userapps/showaddr.rs
+++ os/userapps/showaddr.rs
@@ -3,4 +3,9 @@ fn main(){
     //&a先转成raw指针，然后再把指针转成usize，这样就可以显示地址了。
     let addr = &a as *const i32 as usize;
     println!("addr：0x{:X}",addr);
+	let p =  addr as *const u32 ;
+	unsafe{ 
+      let x = *p;
+      println!("addr:{:?}",x);
+    }
 }
