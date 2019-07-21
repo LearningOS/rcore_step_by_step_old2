f9758dc37a09019267566d5fd6b9cf0532261090
chyyuu <yuchen@mail.tsinghua.edu.cn>
2019-07-21 12:45:35 +0800

    Hello World! -- step 1：显示字符

--- os/src/main.rs
+++ os/src/main.rs
@@ -2,15 +2,19 @@
 #![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
-// This function is called on panic.
+use bbl::sbi;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn rust_main() -> ! {
+    for &c in HELLO {
+        sbi::console_putchar(c as usize);
+    }
     loop {}
 }
