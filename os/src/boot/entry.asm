8c42a1c9becb3320df52c3a5127b641a62815404
Runji Wang <wangrunji0408@163.com>
2019-08-14 16:50:07 +0800

    将内核虚地址移动到 0xC0000000

--- os/src/boot/entry.asm
+++ os/src/boot/entry.asm
@@ -1,13 +1,26 @@
     .section .text.entry
     .globl _start
 _start:
-    # 1. enable paging
+    # 1.1 enable paging
     # satp = (1 << 31) | PPN(boot_page_table_sv32)
     lui     t0, %hi(boot_page_table_sv32)
+    li      t1, 0xC0000000 - 0x80000000
+    sub     t0, t0, t1
     srli    t0, t0, 12
     li      t1, 1 << 31
     or      t0, t0, t1
     csrw    satp, t0
+    sfence.vma
+
+    # 1.2 update PC to 0xCxxxxxxx
+    lui     t0, %hi(remove_identity_map)
+    addi    t0, t0, %lo(remove_identity_map)
+    jr      t0
+
+    # 1.3 remove identity map
+remove_identity_map:
+    lui     t0, %hi(boot_page_table_sv32_top)
+    sw      zero, (-4 * 511)(t0)
     sfence.vma
 
     # 2. setup stack pointer
@@ -27,8 +40,11 @@ bootstacktop:
     .section .data
     .align 12
 boot_page_table_sv32:
-    .zero 4 * 512
-    # 0x80000000 -> 0x80000000 (4M * 2)
-    .word (0x80000 << 10) | 0xcf # VRWXAD
+    .zero 4 * 513
+    # 0x80400000 -> 0x80400000 (4M)
     .word (0x80400 << 10) | 0xcf # VRWXAD
-    .zero 4 * 510
+    .zero 4 * 255
+    # 0xC0400000 -> 0x80400000 (4M)
+    .word (0x80400 << 10) | 0xcf # VRWXAD
+    .zero 4 * 254
+boot_page_table_sv32_top:
