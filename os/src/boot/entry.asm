8fde9a10721083b8e3be3459f47c9e4d8204096b
Runji Wang <wangrunji0408@163.com>
2019-08-14 16:29:23 +0800

    建立最简单的页表，开启分页

--- os/src/boot/entry.asm
+++ os/src/boot/entry.asm
@@ -1,9 +1,20 @@
     .section .text.entry
     .globl _start
 _start:
+    # 1. enable paging
+    # satp = (1 << 31) | PPN(boot_page_table_sv32)
+    lui     t0, %hi(boot_page_table_sv32)
+    srli    t0, t0, 12
+    li      t1, 1 << 31
+    or      t0, t0, t1
+    csrw    satp, t0
+    sfence.vma
+
+    # 2. setup stack pointer
     lui sp, %hi(bootstacktop)
 
-    call rust_main
+    # 3. call rust_main
+    call    rust_main
 
     .section .bss.stack
     .align 12  #PGSHIFT
@@ -12,3 +23,12 @@ bootstack:
     .space 4096 * 16
     .global bootstacktop
 bootstacktop:
+
+    .section .data
+    .align 12
+boot_page_table_sv32:
+    .zero 4 * 512
+    # 0x80000000 -> 0x80000000 (4M * 2)
+    .word (0x80000 << 10) | 0xcf # VRWXAD
+    .word (0x80400 << 10) | 0xcf # VRWXAD
+    .zero 4 * 510
